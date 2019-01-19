5c9f809616c9d4cb43ed04c75daf55fe494c94ce
Th3BFG <th3bfg@gmail.com>
2019-01-18 22:55:45 -0500

    Fixed CORS and server response for website testing locally.

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 
 // Setup CORS
 const corsOptions = {
-    origin: 'http://erbapps.com',
+    origin: 'http://localhost:4200',
     optionsSuccessStatus: 200 // IE11, various SmartTVs choke on 204 
 }
   
@@ -19,15 +19,13 @@ app.route('/api/posts').get((req, res) => {
     // pull list of blogPosts
     // return
     console.log(req); // for testing.
-    res.send({
-        posts: [{id: '1', date_posted: Date.now(), user: 'Ben', post_subject: 'Test Post 1', post_body: 'Test Body 1 with some content.'},
+    res.send([{id: '1', date_posted: Date.now(), user: 'Ben', post_subject: 'Test Post 1', post_body: 'Test Body 1 with some content.'},
         {id: '2', date_posted: Date.now(), user: 'Ben', post_subject: 'Test Post 2', post_body: 'Test Body 2 with some content.'},
         {id: '3', date_posted: Date.now(), user: 'Ben', post_subject: 'Test Post 3', post_body: 'Test Body 3 with some content.'},
         {id: '4', date_posted: Date.now(), user: 'Ben', post_subject: 'Test Post 4', post_body: 'Test Body 4 with some content.'},
         {id: '5', date_posted: Date.now(), user: 'Ben', post_subject: 'Test Post 5', post_body: 'Test Body 5 with some content.'},
-        {id: '6', date_posted: Date.now(), user: 'Ben', post_subject: 'Test Post 6', post_body: 'Test Body 6 with some content.'},
-        ]
-    });
+        {id: '6', date_posted: Date.now(), user: 'Ben', post_subject: 'Test Post 6', post_body: 'Test Body 6 with some content. Courtney does care'},
+    ]);
 });
 
 app.route('/api/posts/:id').get((req, res) => {
